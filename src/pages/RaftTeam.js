082ca356de1e81c223105509ecb7cc01be2227af
razvigor <kamaradsasa@gmail.com>
2022-05-26 14:31:24 +0200

    pagination

--- src/pages/RaftTeam.js
+++ src/pages/RaftTeam.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import axios from 'axios';
 import ExcerptPost from '../components/ExcerptPost';
+import Pagination from '../components/pagination/Pagination';
 import './RaftTeam.scss';
-
+const pageSize = 10;
 const RaftTeam = () => {
 	const [posts, setPosts] = useState(null);
-	const [page, setPage] = useState(1);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(1);
+
 	const ref = useRef(true);
 	useEffect(() => {
 		return function () {
@@ -16,17 +19,20 @@ const RaftTeam = () => {
 		if (ref) {
 			axios
 				.get(
-					`http://mbportfolio.info/api/wp-json/wp/v2/posts?categories=5&_embed&per_page=6&page=${page}`
+					`http://mbportfolio.info/api/wp-json/wp/v2/posts?categories=5&_embed&per_page=6&page=${currentPage}`
 				)
 				.then((res) => {
 					console.log(res);
 					setPosts(res.data);
+					setTotalPages(parseInt(res.headers['x-wp-totalpages']));
 				})
 				.catch((err) => {
 					console.log(err);
 				});
+		} else {
+			return;
 		}
-	}, [page, setPage]);
+	}, [currentPage, setCurrentPage]);
 	const content =
 		posts !== null
 			? posts.map((item) => {
@@ -34,11 +40,7 @@ const RaftTeam = () => {
 						<ExcerptPost
 							key={item.id}
 							excerpt={item.excerpt.rendered}
-							imgSrc={
-								item['_embedded']['wp:featuredmedia'][0]['media_details'][
-									'sizes'
-								]['medium']['source_url']
-							}
+							imgSrc={item['_embedded']['wp:featuredmedia'][0]['source_url']}
 							title={item.title.rendered}
 							urlToPost={`/blog/${item.id}`}
 						/>
@@ -49,6 +51,13 @@ const RaftTeam = () => {
 		<div className='raft-team'>
 			<h1>Raft Tim</h1>
 			<div className='container'>{content}</div>
+			<Pagination
+				className='pagination-bar'
+				currentPage={currentPage}
+				totalCount={totalPages}
+				pageSize={pageSize}
+				onPageChange={(page) => setCurrentPage(page)}
+			/>
 		</div>
 	);
 };
