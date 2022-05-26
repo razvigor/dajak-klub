749d5085279e19f389c7756b79b51e30e19f661b
razvigor <kamaradsasa@gmail.com>
2022-05-26 13:44:55 +0200

    raft-team-ispravka

--- src/pages/RaftTeam.js
+++ src/pages/RaftTeam.js
@@ -1,38 +1,56 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import ExcerptPost from '../components/ExcerptPost';
-import './RaftTeam.scss'
+import './RaftTeam.scss';
 
 const RaftTeam = () => {
-  const [posts, setPosts] = useState(null)
-  const ref = useRef(true)
-  useEffect(() => {
-    return function() {
-      ref.current = false;
-    }
-  })
-  useEffect(() => {
-    if (ref) {
-      axios.get('http://mbportfolio.info/api/wp-json/wp/v2/posts?categories=5')
-      .then(res => {
-        console.log(res)
-        setPosts(res.data)
-      }).catch(err => {
-        console.log(err)
-      })
-    }
-  }, [])
-  const content = posts !== null ? posts.map(item => {
-  return <ExcerptPost key={item.id} excerpt={item.excerpt.rendered} imgSrc={`http://mbportfolio.info/api/wp-json/wp/v2/media/${item.featured_media}`} title={item.title.rendered} urlToPost={`/blog/${item.id}`} />
-  }) : null;
-  return (
-    <div className='raft-team'>
-      <h1>Raft Tim</h1>
-      <div className='container'>
-      {content}
-      </div>
-      </div>
-  )
-}
+	const [posts, setPosts] = useState(null);
+	const [page, setPage] = useState(1);
+	const ref = useRef(true);
+	useEffect(() => {
+		return function () {
+			ref.current = false;
+		};
+	});
+	useEffect(() => {
+		if (ref) {
+			axios
+				.get(
+					`http://mbportfolio.info/api/wp-json/wp/v2/posts?categories=5&_embed&per_page=6&page=${page}`
+				)
+				.then((res) => {
+					console.log(res);
+					setPosts(res.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	}, [page, setPage]);
+	const content =
+		posts !== null
+			? posts.map((item) => {
+					return (
+						<ExcerptPost
+							key={item.id}
+							excerpt={item.excerpt.rendered}
+							imgSrc={
+								item['_embedded']['wp:featuredmedia'][0]['media_details'][
+									'sizes'
+								]['medium']['source_url']
+							}
+							title={item.title.rendered}
+							urlToPost={`/blog/${item.id}`}
+						/>
+					);
+			  })
+			: null;
+	return (
+		<div className='raft-team'>
+			<h1>Raft Tim</h1>
+			<div className='container'>{content}</div>
+		</div>
+	);
+};
 
-export default RaftTeam
+export default RaftTeam;
